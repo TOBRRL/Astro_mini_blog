363f4d12675f20dbe33c20f5ea7030d1edc4d089
Administrator <Administrator@KIVIVI>
2026-04-17 11:21:10 +0800

    v1.0

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "ACCIRE";
+export const SITE_TITLE = "ACCIRE's Blog";
 export const SITE_DESCRIPTION =
   "A blog driven by interest.";
 export const SITE_URL = "https://accire.cn/";
